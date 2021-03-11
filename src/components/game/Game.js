987e8c63adf4ebd840965ed98cf232e10d44cc12
jhlov <jhlovv@gmail.com>
2021-03-11 20:02:38 +0900

    랜덤한 숫자 25개 뿌리기

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,9 +1,27 @@
 import "./Game.scss";
 
+import React, { useEffect, useState } from "react";
+
 import Cell from "components/game/Cell";
-import React from "react";
 
 const Game = () => {
+  // 숫자판
+  const [numberList, setNumberList] = useState([]);
+
+  useEffect(() => {
+    let arr = new Array(25);
+    for (let i = 0; i < arr.length; ++i) {
+      arr[i] = getRandomNumber();
+    }
+
+    setNumberList(arr);
+  }, []);
+
+  function getRandomNumber() {
+    const arr = [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
+    return arr[parseInt(Math.random() * arr.length)];
+  }
+
   return (
     <div>
       {/* 상단 */}
@@ -29,13 +47,9 @@ const Game = () => {
 
       {/* 하단 */}
       <div className="board mt-40">
-        <Cell value={1} />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
+        {numberList.map((number, index) => (
+          <Cell value={number} key={index} />
+        ))}
       </div>
     </div>
   );
