baf4d464b5054440b459c9087a1aa3aaf2a798ab
jhlov <jhlovv@gmail.com>
2021-03-15 18:35:45 +0900

    github page 배포 테스트

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,21 @@
 import "styles/style.scss";
 import "./App.scss";
 
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Game from "components/game/Game";
 import Menu from "components/menu/Menu";
 import Rank from "components/rank/Rank";
-import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Route path="/" component={Menu} exact={true} />
-        <Route path="/game" component={Game} />
-        <Route path="/rank" component={Rank} />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Route path="/" component={Menu} exact={true} />
+          <Route path="/game" component={Game} />
+          <Route path="/rank" component={Rank} />
+        </BrowserRouter>
       </div>
     </div>
   );
